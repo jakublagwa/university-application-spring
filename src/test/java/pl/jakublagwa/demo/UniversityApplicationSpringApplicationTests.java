6ea4232f9e9f1d00a74869de5040283fb6760916
Jakub Łągwa <kklagwa@gmail.com>
2023-01-18 14:09:04 +0100

    first commit

---
+++ src/test/java/pl/jakublagwa/demo/UniversityApplicationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakublagwa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
