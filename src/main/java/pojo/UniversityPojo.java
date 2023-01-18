b9758f0d709f1e08df52809ea859a03796a5f3e5
Jakub Łągwa <kklagwa@gmail.com>
2023-01-18 15:28:39 +0100

    adding pojo

--- src/main/java/pojo/UniversityPojo.java
+++ src/main/java/pojo/UniversityPojo.java
@@ -5,7 +5,36 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+@Entity
 public class UniversityPojo {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int age;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age=age;
+	}
 }
--- src/main/java/pojo/UserPojo.java
+++ src/main/java/pojo/UserPojo.java
@@ -1,5 +1,17 @@
 package pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class UserPojo {
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 
 }
