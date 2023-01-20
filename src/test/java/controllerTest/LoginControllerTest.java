c8ab7a98adfa33209ff0df720139dea912432f92
Jakub Łągwa <kklagwa@gmail.com>
2023-01-20 10:49:30 +0100

    created my first junit spring boot test, im so proud

--- src/test/java/controllerTest/LoginControllerTest.java
+++ src/test/java/controllerTest/LoginControllerTest.java
@@ -1,22 +1,27 @@
 package controllerTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import controller.LoginController;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginControllerTest {
 
 	@Autowired
-	private LoginController loginController;
+	private MockMvc mockMvc;
 	
 	@Test
-	public void testMethod() {
-		
+	public void logoutTest() throws Exception {
+		mockMvc.perform(get("/session/logout"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/site/logout"));
 	}
 }
